0822f7dd01f38ff5a3a1265e1609269b571842dd
serg470 <serg470@yandex.ru>
2019-06-07 17:24:01 +0300

    First commit

--- fabric/tasks/srv/elk.py
+++ fabric/tasks/srv/elk.py
@@ -1,5 +1,7 @@
 from lib.files import *
 from lib.docker import *
+from io import BytesIO
+
 
 @task()
 def deploy():
@@ -7,15 +9,27 @@ def deploy():
     Deploy ELK
     :return:
     '''
+    sudo("clean")
     if exists('/opt/elk/', use_sudo=True):
         sudo("rm -rf /opt/elk/")
     sudo("git clone https://github.com/serg470/elk.git /opt/elk/")
-    sudo("export ELK_VERSION=7.0.1")
     sudo("docker-compose -f /opt/elk/docker-compose.yml up -d")
     sudo("sleep 1m")
     sudo("docker-compose -f /opt/elk/docker-compose.yml exec -T elasticsearch 'bin/elasticsearch-setup-passwords' auto --batch > /opt/elk/passwd")
     sudo("cat /opt/elk/passwd | grep = | tail -1 | cut -d' ' -f4 > /opt/elk/elastic")
+    passwd = (read_file("/opt/elk/elastic")).rstrip()
+    sudo("sed -i 's/changeme/%s/g' /opt/elk/kibana/config/kibana.yml" % passwd)
+    sudo("sed -i 's/changeme/%s/g' /opt/elk/logstash/config/logstash.yml" % passwd)
+    sudo("sed -i 's/changeme/%s/g' /opt/elk/logstash/pipeline/logstash.conf" % passwd)
+    sudo("docker-compose -f /opt/elk/docker-compose.yml restart")
+    sudo("sleep 1m")
+#    sudo("curl -X POST 'http://extproxy.intertransl.com:5601/api/spaces/space' -u elastic:%s -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d\'{"id"}\'" % passwd)
     put_file("usr", "elk", "bin", "elk.sh", mode=0755)
     sudo("/usr/elk/bin/elk.sh")
-    sudo("docker-compose -f /opt/elk/docker-compose.yml restart")
 
+
+
+def read_file(file_path, encoding='utf-8'):
+    io_obj = BytesIO()
+    get(file_path, io_obj)
+    return io_obj.getvalue().decode(encoding)
--- files/usr/elk/bin/elk.sh
+++ files/usr/elk/bin/elk.sh
@@ -1,16 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-sed -i "s/changeme/$(cat /opt/elk/elastic)/g" /opt/elk/kibana/config/kibana.yml
-sed -i "s/changeme/$(cat /opt/elk/elastic)/g" /opt/elk/logstash/config/logstash.yml
-sed -i "s/changeme/$(cat /opt/elk/elastic)/g" /opt/elk/logstash/pipeline/logstash.conf
+curl -X POST "http://extproxy.intertransl.com:5601/api/spaces/space" -u elastic:$(cat /opt/elk/elastic) -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d '{"id": "linux-servers","name": "Linux Servers","description" : "This is the Linux Servers Space","color": "#aabbcc","initials": "LS"}'
 
-curl -X POST "http://localhost:5601/api/spaces/space" -u elastic:$(cat /opt/elk/elastic) -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'{"id": "linux-servers","name": "Linux
-
-touch /opt/elk/filebeat.yml
-echo "output.elasticsearch:" >> /opt/elk/filebeat.yml
-echo " hosts: ["$HOSTNAME".intertransl.com:9200\"]" >> /opt/elk/filebeat.yml
-echo " username: \"elastic\"" >> /opt/elk/filebeat.yml
-echo " password: "$(cat /opt/elk/elastic) >> /opt/elk/filebeat.yml
-echo "setup.kibana:" >> /opt/elk/filebeat.yml
-echo " host: "$HOSTNAME".intertransl.com:5601\"" >> /opt/elk/filebeat.yml
-echo " space.id: \"linux-servers\"" >> /opt/elk/filebeat.yml
+touch /opt/elk/fb.conf
+echo "output.elasticsearch:" >> /opt/elk/fb.conf
+echo " hosts: ["$HOSTNAME".intertransl.com:9200\"]" >> /opt/elk/fb.conf
+echo " username: \"elastic\"" >> /opt/elk/fb.conf
+echo " password: "$(cat /opt/elk/elastic) >> /opt/elk/fb.conf
+echo "setup.kibana:" >> /opt/elk/fb.conf
+echo " host: "$HOSTNAME".intertransl.com:5601\"" >> /opt/elk/fb.conf
+echo " space.id: \"linux-servers\"" >> /opt/elk/fb.conf
